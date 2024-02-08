71eeaeb734f2a9887d6c5948d22926970fa10589
Nikunj2303 <142402711+Nikunj2303@users.noreply.github.com>
2024-02-08 14:50:46 +0530

    Add files via upload

---
+++ sales/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'Sale Receipt Status',
+    'version': '17.0.0.1',
+    'license': 'LGPL-3',
+    'sequence': 1,
+    'category': 'Sales',
+    'depends': ['product', 'purchase', 'sale'],
+    'data': ['view\purchase_order_views.xml'],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
